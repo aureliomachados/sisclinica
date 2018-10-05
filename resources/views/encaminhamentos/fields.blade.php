b61ef1a3019ffd60735e0b4259c59d0b5145f832
Aurelio <aureliomachados@gmail.com>
2018-10-05 09:51:52 -0300

    Add Laravel Debugbar

--- resources/views/encaminhamentos/fields.blade.php
+++ resources/views/encaminhamentos/fields.blade.php
@@ -1,6 +1,6 @@
 <!-- Paciente Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('paciente', 'Paciente:') !!}
+    {!! Form::label('paciente', 'Nome do paciente:') !!}
     {!! Form::text('paciente', null, ['class' => 'form-control']) !!}
 </div>
 
