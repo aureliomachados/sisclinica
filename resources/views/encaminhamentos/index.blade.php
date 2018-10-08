23181d9aded92e6e913783416ed3e615f2b30bfd
Aurelio <aureliomachados@gmail.com>
2018-10-08 16:36:25 -0300

    edit fields

--- resources/views/encaminhamentos/index.blade.php
+++ resources/views/encaminhamentos/index.blade.php
@@ -4,7 +4,7 @@
     <section class="content-header">
         <h1 class="pull-left">Encaminhamentos</h1>
         <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('encaminhamentos.create') !!}">Add New</a>
+           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('encaminhamentos.create') !!}">Adicionar Novo</a>
         </h1>
     </section>
     <div class="content">
